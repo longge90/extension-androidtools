9baefba3ffabc3447e68afc556d8e6a90c291f6b
longge90 <151552445+longge90@users.noreply.github.com>
2024-08-07 23:12:06 +0800

    first commit

---
+++ android/Tools.hx
@@ -0,0 +1,229 @@
+package android;
+
+#if (!android && !native)
+#error 'extension-androidtools is not supported on your current platform'
+#end
+import android.Permissions;
+import haxe.io.Path;
+import lime.app.Event;
+import lime.system.JNI;
+import lime.utils.Log;
+
+class Tools
+{
+	public static function openDirectoryPicker(?requestCode:Int = 1):Void
+	{
+		openDirectoryPicker_jni(requestCode);
+	}
+
+	public static function getUriPath(uriString:String):String
+	{
+		return getUriPath_jni(uriString);
+	}
+
+	public static function registerUriAccess(uri:String):Void
+	{
+		registerUriAccess_jni(uri);
+	}
+
+	/**
+	 * Prompt the user to install a specific APK file.
+	 */
+	public static function installPackage(path:String):Void
+	{
+		if (!installPackage_jni(path))
+			Log.warn('"REQUEST_INSTALL_PACKAGES" permission and "Install apps from external sources" setting must be granted to this app in order to install a ${Path.extension(path).toUpperCase()} file.');
+	}
+
+	/**
+	 * Adds the security flag to application's window.
+	 */
+	public static inline function enableAppSecure():Void
+	{
+		enableAppSecure_jni();
+	}
+
+	/**
+	 * Clears the security flag from application's window.
+	 */
+	public static inline function disableAppSecure():Void
+	{
+		disableAppSecure_jni();
+	}
+
+	/**
+	 * Launches a app by the `packageName`.
+	 */
+	public static inline function launchPackage(packageName:String, requestCode:Int = 1):Void
+	{
+		launchPackage_jni(packageName, requestCode);
+	}
+
+	/**
+	 * Launches a app by the `packageName`.
+	 */
+	public static function showAlertDialog(title:String, message:String, ?positiveButton:ButtonData, ?negativeButton:ButtonData):Void
+	{
+		if (positiveButton == null)
+			positiveButton = {name: null, func: null};
+
+		if (negativeButton == null)
+			negativeButton = {name: null, func: null};
+
+		showAlertDialog_jni(title, message, positiveButton.name, new ButtonListener(positiveButton.func), negativeButton.name,
+			new ButtonListener(negativeButton.func));
+	}
+
+	/**
+	 * @return `true` If the device have root.
+	 */
+	public static inline function isRooted():Bool
+	{
+		return isRooted_jni();
+	}
+
+	/**
+	 * @return `true` If the device has Dolby Atmos.
+	 */
+	public static inline function isDolbyAtmos():Bool
+	{
+		return isDolbyAtmos_jni();
+	}
+
+	/**
+	 * Shows a minimal notification with a title and message.
+	 */
+	public static inline function showNotification(title:String, message:String, ?channelID:String = 'unknown_channel',
+			?channelName:String = 'Unknown Channel', ?ID:Int = 1):Void
+	{
+		showNotification_jni(title, message, channelID, channelName, ID);
+	}
+
+	/**
+	 * Sets Activity's Title by the `title`.
+	 */
+	public static inline function setActivityTitle(title:String):Bool
+	{
+		return setActivityTitle_jni(title);
+	}
+
+	/**
+	 * Minimizes app's window.
+	 */
+	public static inline function minimizeWindow():Void
+	{
+		minimizeWindow_jni();
+	}
+
+	/**
+	 * @return whether the device is running Android TV.
+	 */
+	public static inline function isAndroidTV():Bool
+	{
+		return isAndroidTV_jni();
+	}
+
+	/**
+	 * @return whether the device is a Tablet.
+	 */
+	public static inline function isTablet():Bool
+	{
+		return isTablet_jni();
+	}
+
+	/**
+	 * @return whether the device is a ChromeBook.
+	 */
+	public static inline function isChromebook():Bool
+	{
+		return isChromebook_jni();
+	}
+
+	/**
+	 * @return whether the device is running in Dex Mode.
+	 */
+	public static inline function isDeXMode():Bool
+	{
+		return isDexMode_jni();
+	}
+
+	@:noCompletion
+	private static var openDirectoryPicker_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'openDirectoryPicker', '(I)V');
+
+	@:noCompletion
+	private static var getUriPath_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'getUriPath', '(Ljava/lang/String;)Ljava/lang/String;');
+
+	@:noCompletion
+	private static var registerUriAccess_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'registerUriAccess', '(Ljava/lang/String;)V');
+
+	@:noCompletion
+	private static var installPackage_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'installPackage', '(Ljava/lang/String;)Z');
+
+	@:noCompletion
+	private static var enableAppSecure_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'enableAppSecure', '()V');
+
+	@:noCompletion
+	private static var disableAppSecure_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'disableAppSecure', '()V');
+
+	@:noCompletion
+	private static var launchPackage_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'launchPackage', '(Ljava/lang/String;I)V');
+
+	@:noCompletion
+	private static var showAlertDialog_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'showAlertDialog',
+		'(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Lorg/haxe/lime/HaxeObject;Ljava/lang/String;Lorg/haxe/lime/HaxeObject;)V');
+
+	@:noCompletion
+	private static var isRooted_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'isRooted', '()Z');
+
+	@:noCompletion
+	private static var isDolbyAtmos_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'isDolbyAtmos', '()Z');
+
+	@:noCompletion
+	private static var showNotification_jni:Dynamic = JNI.createStaticMethod('org/haxe/extension/Tools', 'showNotification',
+		'(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)V');
+
+	@:noCompletion
+	private static var setActivityTitle_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'setActivityTitle', '(Ljava/lang/String;)Z');
+
+	@:noCompletion
+	private static var minimizeWindow_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'minimizeWindow', '()V');
+
+	@:noCompletion
+	private static var isAndroidTV_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'isAndroidTV', '()Z');
+
+	@:noCompletion
+	private static var isTablet_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'isTablet', '()Z');
+
+	@:noCompletion
+	private static var isChromebook_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'isChromebook', '()Z');
+
+	@:noCompletion
+	private static var isDexMode_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'isDeXMode', '()Z');
+}
+
+@:noCompletion
+private typedef ButtonData =
+{
+	name:String,
+	func:Void->Void
+}
+
+@:noCompletion
+private class ButtonListener #if (lime >= "8.0.0") implements JNISafety #end
+{
+	private var onClickEvent:Event<Void->Void> = new Event<Void->Void>();
+
+	public function new(clickCallback:Void->Void):Void
+	{
+		if (clickCallback != null)
+			onClickEvent.add(clickCallback);
+	}
+
+	#if (lime >= "8.0.0")
+	@:runOnMainThread
+	#end
+	public function onClick():Void
+	{
+		onClickEvent.dispatch();
+	}
+}
---
+++ project/androidtools/src/main/java/org/haxe/extension/Tools.java
@@ -0,0 +1,469 @@
+package org.haxe.extension;
+
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.media.MediaCodecList;
+import android.media.MediaFormat;
+import android.net.Uri;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.util.ArrayMap;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.core.content.FileProvider;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.haxe.extension.Extension;
+import org.haxe.lime.HaxeObject;
+import android.content.ContentResolver;
+import android.os.Environment;
+
+/* 
+	You can use the Android Extension class in order to hook
+	into the Android activity lifecycle. This is not required
+	for standard Java code, this is designed for when you need
+	deeper integration.
+
+	You can access additional references from the Extension class,
+	depending on your needs:
+
+	- Extension.assetManager (android.content.res.AssetManager)
+	- Extension.callbackHandler (android.os.Handler)
+	- Extension.mainActivity (android.app.Activity)
+	- Extension.mainContext (android.content.Context)
+	- Extension.mainView (android.view.View)
+
+	You can also make references to static or instance methods
+	and properties on Java classes. These classes can be included 
+	as single files using <java path="to/File.java" /> within your
+	project, or use the full Android Library Project format (such
+	as this example) in order to include your own AndroidManifest
+	data, additional dependencies, etc.
+
+	These are also optional, though this example shows a static
+	function for performing a single task, like returning a value
+	back to Haxe from Java.
+*/
+public class Tools extends Extension
+{
+	public static final String LOG_TAG = "Tools";
+
+	public static HaxeObject cbObject;
+
+	public static Gson gson;
+
+	public static void initCallBack(final HaxeObject cbObject)
+	{
+		Tools.cbObject = cbObject;
+	}
+
+	public static void openDirectoryPicker(final int requestCode)
+	{
+		Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+		intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+       	mainActivity.startActivityForResult(intent, requestCode);
+    }
+
+	public static void registerUriAccess(String uriString)
+	{
+		ContentResolver contentResolver = mainContext.getContentResolver();
+		contentResolver.takePersistableUriPermission(Uri.parse(uriString), Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+	}
+
+	public static String getUriPath(String uriString)
+	{
+		Uri uri = Uri.parse(uriString);
+		return Environment.getExternalStorageDirectory() + "/" + uri.getPath().split(":")[1];
+    }
+
+	public static String[] getGrantedPermissions()
+	{
+		List<String> granted = new ArrayList<String>();
+
+		try
+		{
+			PackageInfo info = (PackageInfo) mainContext.getPackageManager().getPackageInfo(packageName, PackageManager.GET_PERMISSIONS);
+
+			for (int i = 0; i < info.requestedPermissions.length; i++)
+				if ((info.requestedPermissionsFlags[i] & PackageInfo.REQUESTED_PERMISSION_GRANTED) != 0)
+					granted.add(info.requestedPermissions[i]);
+		}
+		catch (Exception e)
+		{
+			Log.e(LOG_TAG, e.toString());
+		}
+
+		return granted.toArray(new String[granted.size()]);
+	}
+
+	public static void makeToastText(final String message, final int duration, final int gravity, final int xOffset, final int yOffset)
+	{
+		mainActivity.runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					Toast toast = Toast.makeText(mainContext, message, duration);
+
+					if (gravity >= 0)
+						toast.setGravity(gravity, xOffset, yOffset);
+
+					toast.show();
+				}
+				catch (Exception e)
+				{
+					Log.e(LOG_TAG, e.toString());
+				}
+			}
+		});
+	}
+
+	public static void showAlertDialog(final String title, final String message, final String positiveLabel, final HaxeObject positiveObject, final String negativeLabel, final HaxeObject negativeObject)
+	{
+		mainActivity.runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					AlertDialog.Builder builder = new AlertDialog.Builder(mainContext, android.R.style.Theme_Material_Dialog_Alert);
+
+					if (title != null)
+						builder.setTitle(title);
+
+					TextView messageView = new TextView(mainContext);
+					messageView.setPadding(20, 20, 20, 20);
+					messageView.setText(message);
+
+					ScrollView scrollView = new ScrollView(mainContext);
+					scrollView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 300));
+					scrollView.addView(messageView);
+
+					builder.setView(scrollView);
+
+					if (positiveLabel != null)
+					{
+						builder.setPositiveButton(positiveLabel, new DialogInterface.OnClickListener()
+						{
+							@Override
+							public void onClick(DialogInterface dialog, int which)
+							{
+								dialog.dismiss();
+
+								if (positiveObject != null)
+									positiveObject.call("onClick", new Object[] {});
+							}
+						});
+					}
+				
+					if (negativeLabel != null)
+					{
+						builder.setNegativeButton(negativeLabel, new DialogInterface.OnClickListener()
+						{
+							@Override
+							public void onClick(DialogInterface dialog, int which)
+							{
+								dialog.dismiss();
+
+								if (negativeObject != null)
+									negativeObject.call("onClick", new Object[] {});
+							}
+						});
+					}
+
+					builder.setCancelable(false);
+					builder.create().show();
+				}
+				catch (Exception e)
+				{
+					Log.e(LOG_TAG, e.toString());
+				}
+			}
+		});
+	}
+
+	public static boolean installPackage(final String path)
+	{
+		try
+		{
+			boolean retVal = true;
+
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+				retVal = mainContext.getPackageManager().canRequestPackageInstalls();
+
+			File file = new File(path);
+
+			Uri contentUri;
+
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+				contentUri = FileProvider.getUriForFile(mainContext, packageName + ".provider", file);
+			else
+				contentUri = Uri.fromFile(file);
+
+			if (file.exists())
+			{
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+				mainContext.startActivity(intent);
+			}
+			else 
+				Log.e(LOG_TAG, "Attempted to install a application package from " + file.getAbsolutePath() + " but the file dosen't exist.");
+
+			return retVal;
+		}
+		catch (Exception e)
+		{
+			Log.e(LOG_TAG, e.toString());
+
+			return false;
+		}
+	}
+
+	public static void enableAppSecure()
+	{
+		mainActivity.runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					mainActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+				}
+				catch (Exception e)
+				{
+					Log.e(LOG_TAG, e.toString());
+				}
+			}
+		});
+	}
+
+	public static void disableAppSecure()
+	{
+		mainActivity.runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					mainActivity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+				}
+				catch (Exception e)
+				{
+					Log.e(LOG_TAG, e.toString());
+				}
+			}
+		});
+	}
+
+	public static void launchPackage(final String packageName, final int requestCode)
+	{
+		try
+		{
+			mainActivity.startActivityForResult(mainActivity.getPackageManager().getLaunchIntentForPackage(packageName), requestCode);
+		}
+		catch (Exception e)
+		{
+			Log.e(LOG_TAG, e.toString());
+		}
+	}
+
+	public static void requestSetting(final String setting, final int requestCode)
+	{
+		try
+		{
+			Intent intent = new Intent(setting);
+			intent.setData(Uri.fromParts("package", packageName, null));
+			mainActivity.startActivityForResult(intent, requestCode);
+		}
+		catch (Exception e)
+		{
+			Log.e(LOG_TAG, e.toString());
+		}
+	}
+
+	public static boolean isRooted()
+	{
+		try
+		{
+			Process execute = Runtime.getRuntime().exec("su");
+
+			execute.waitFor();
+
+			if (execute.exitValue() != 255)
+				return true;
+		}
+		catch (Exception e)
+		{
+			Log.e(LOG_TAG, e.toString());
+		}
+
+		return false;
+	}
+
+	public static boolean isDolbyAtmos()
+	{
+		try
+		{
+			MediaFormat format = new MediaFormat();
+
+			format.setString(MediaFormat.KEY_MIME, "audio/eac3-joc"); // or "audio/ac4"
+
+			MediaCodecList codecList = new MediaCodecList(MediaCodecList.ALL_CODECS);
+
+			if (codecList.findDecoderForFormat(format) != null)
+				return true;
+		}
+		catch (Exception e)
+		{
+			Log.e(LOG_TAG, e.toString());
+		}
+
+		return false;
+	}
+
+	public static void showNotification(final String title, final String message, final String channelID, final String channelName, final int ID)
+	{
+		mainActivity.runOnUiThread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					NotificationManager notificationManager = (NotificationManager) mainContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+						notificationManager.createNotificationChannel(new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT));
+
+					Notification.Builder builder = new Notification.Builder(mainContext, channelID);
+					builder.setAutoCancel(true);
+					builder.setContentTitle(title);
+					builder.setContentText(message);
+					builder.setDefaults(Notification.DEFAULT_ALL);
+					builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+					builder.setWhen(System.currentTimeMillis());
+					notificationManager.notify(ID, builder.build());
+				}
+				catch (Exception e)
+				{
+					Log.e(LOG_TAG, e.toString());
+				}
+			}
+		});
+	}
+
+	public static File getFilesDir()
+	{
+		return mainContext.getFilesDir();
+	}
+
+	public static File getExternalFilesDir(final String type)
+	{
+		return mainContext.getExternalFilesDir(type);
+	}
+
+	public static File getCacheDir()
+	{
+		return mainContext.getCacheDir();
+	}
+
+	public static File getCodeCacheDir()
+	{
+		return mainContext.getCodeCacheDir();
+	}
+
+	public static File getNoBackupFilesDir()
+	{
+		return mainContext.getNoBackupFilesDir();
+	}
+
+	public static File getExternalCacheDir()
+	{
+		return mainContext.getExternalCacheDir();
+	}
+
+	public static File getObbDir()
+	{
+		return mainContext.getObbDir();
+	}
+
+	public static BatteryManager getBatteryManager()
+	{
+		return (BatteryManager) mainContext.getSystemService(Context.BATTERY_SERVICE);
+	}
+
+	/**
+	 * Called when an activity you launched exits, giving you the requestCode 
+	 * you started it with, the resultCode it returned, and any additional data 
+	 * from it.
+	 */
+	@Override
+	public boolean onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if (cbObject != null)
+		{
+			ArrayMap<String, Object> content = new ArrayMap<String, Object>();
+
+			content.put("requestCode", requestCode);
+			content.put("resultCode", resultCode);
+
+			if (data != null && data.getData() != null)
+				content.put("uri", data.getData().toString());
+
+			if (gson == null)
+				gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+
+			cbObject.call("onActivityResult", new Object[] {
+				gson.toJson(content)
+			});
+		}
+
+		return true;
+	}
+
+	/**
+	 * Callback for the result from requesting permissions.
+	 */
+	@Override
+	public boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+	{
+		if (cbObject != null)
+		{
+			ArrayMap<String, Object> content = new ArrayMap<String, Object>();
+
+			content.put("requestCode", requestCode);
+			content.put("permissions", permissions);
+			content.put("grantResults", grantResults);
+
+			if (gson == null)
+				gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+
+			cbObject.call("onRequestPermissionsResult", new Object[] {
+				gson.toJson(content)
+			});
+		}
+
+		return true;
+	}
+}
